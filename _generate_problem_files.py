467e3c32d0f10c86d3b8745f80d77d370dd07b24
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2023-09-27 14:26:39 +0300

    generate problem files script upgraded

--- _generate_problem_files.py
+++ _generate_problem_files.py
@@ -1,5 +1,12 @@
+import os
+
 # Define the name of the source file and its content
 source_file_name = "withdeadlines-ontime-pfile15-"
+
+# Check if source file exists
+if not os.path.exists(source_file_name):
+    print(f"Error: {source_file_name} does not exist.")
+    exit()
 
 with open(source_file_name, 'r') as source_file:
     content = source_file.read()
@@ -10,4 +17,19 @@ for i in range(1, 101):
     with open(target_file_name, 'w') as target_file:
         target_file.write(content)
 
-print("Files created successfully!")
+# Verification step
+all_files_match = True
+for i in range(1, 101):
+    target_file_name = f"withdeadlines-ontime-pfile15-{i}"
+
+    with open(target_file_name, 'r') as target_file:
+        target_content = target_file.read()
+
+        if target_content != content:
+            print(f"Content mismatch in {target_file_name}")
+            all_files_match = False
+
+if all_files_match:
+    print("Files created and verified successfully!")
+else:
+    print("Some files do not match the original content!")
