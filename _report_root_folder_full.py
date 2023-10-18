0683a6ab03e39c712d49db5d08161d700349689a
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2023-10-18 18:13:34 +0300

    report root folder improved

--- _report_root_folder_full.py
+++ _report_root_folder_full.py
@@ -6,34 +6,47 @@ import re
 # Define the output file name
 output_file_name = "output_full.csv"
 
+# Ensure the directory is clean for the output file, avoiding appending to an old file.
+if os.path.exists(output_file_name):
+    os.remove(output_file_name)
+
 # Write header to output file
-with open(output_file_name, "w") as csvfile:
+with open(output_file_name, "w", newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(["experiment", "disp", "nodisp"])
+    csv_writer.writerow(["File", "disp", "nodisp"])
 
-# Function to process zip files
-def process_zip_file(filename, experiment_name):
-    with tarfile.open(filename, "r:gz") as archive:
-        for member in archive:
-            if member.name.endswith("times.csv"):
-                with archive.extractfile(member) as f:
-                    file_content = f.read().decode("utf-8")
-                    csv_reader = csv.reader(file_content.splitlines())
-                    next(csv_reader)  # Skip header
-                    for row in csv_reader:
-                        disp, nodisp = row[1], row[2]
-                        with open(output_file_name, "a") as csvfile:
-                            csv_writer = csv.writer(csvfile)
-                            csv_writer.writerow([experiment_name, disp, nodisp])
 
-# Start the traversal from current directory
+# Function to process tar.gz files
+def process_tar_file(filepath):
+    with tarfile.open(filepath, "r:gz") as archive:
+        for member in archive.getmembers():  # Use getmembers to ensure it's iterable
+            if member.isreg():  # Ensure it's a regular file, not a folder
+                if member.name.endswith("times.csv"):
+                    with archive.extractfile(member) as f:
+                        file_content = f.read().decode("utf-8")
+                        csv_reader = csv.reader(file_content.splitlines())
+                        headers = next(csv_reader)  # Read the header row
+
+                        # Validate headers if necessary
+                        if headers[:3] == ["File", "disp", "nodisp"]:
+                            for row in csv_reader:
+                                # Extract data
+                                file_name, disp, nodisp = row
+                                with open(output_file_name, "a", newline='') as csvfile:
+                                    csv_writer = csv.writer(csvfile)
+                                    csv_writer.writerow([file_name, disp, nodisp])
+                        else:
+                            print(f"Unexpected columns in {member.name} within {filepath}")
+
+
+# Start the traversal from specified directories
 base_dirs = ["PC1", "PC2"]
 for base in base_dirs:
     for root, dirs, files in os.walk(base):
         for file in files:
             match = re.match(r"(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2})\.tar\.gz$", file)
             if match:
-                experiment_name = match.group(1)
-                process_zip_file(os.path.join(root, file), experiment_name)
+                full_file_path = os.path.join(root, file)
+                process_tar_file(full_file_path)
 
 print(f"Data has been written to {output_file_name}")
