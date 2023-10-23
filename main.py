a2157388c1c62550823c98fd899c5a7b8f8837ee
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2023-10-24 02:39:06 +0300

    iterate from 1 to 21 and back

--- main.py
+++ main.py
@@ -20,8 +20,13 @@ OBJECT = 'crate'
 PLAN_SEARCH_TIMEOUT_SECONDS = 60
 EXPERIMENTS = 100
 PFILE_N = 10
+
+# Define the range for PFILE_N
+PFILE_START = 1
+PFILE_END = 21  # This allows the loop to go up to PFILE_N = 21
+
 PFILE = f"pfile{PFILE_N}"
-FOREST_DEADLINES_ENABLED = False
+FOREST_DEADLINES_ENABLED = True
 
 # Flag to enable or disable parallel processing
 ENABLE_PARALLEL = True
@@ -63,191 +68,199 @@ def remove_folders_and_files():
 
 # Main loop to run everything in an infinite cycle
 while True:
-    print(f"[{datetime.now()}] Starting a new set of experiments...")
-    # Create directories and set permissions
-    for dir_name in ['disp', 'nodisp']:
-        if not os.path.exists(dir_name):
-            os.makedirs(dir_name)
-            os.chmod(dir_name, 0o755)
+    for current_pfile in range(PFILE_START, PFILE_END + 1):  # Looping over the defined range
+        PFILE_N = current_pfile  # Updating the PFILE_N value for this iteration
+        PFILE = f"pfile{PFILE_N}"
 
-    # Directories to parse
-    directories = ['disp', 'nodisp']
+        print(f"[{datetime.now()}] Starting a new set of experiments for PFILE_N = {PFILE_N}...")
 
-    # Output CSV filenames
-    csv_filename = "times.csv"
-    csv_filename_better = "timesDispBetter.csv"
+        print(f"[{datetime.now()}] Starting a new set of experiments...")
+        # Create directories and set permissions
+        for dir_name in ['disp', 'nodisp']:
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+                os.chmod(dir_name, 0o755)
 
-    # Base filename pattern
-    base_filename = f"{PFILE_N}-"
+        # Directories to parse
+        directories = ['disp', 'nodisp']
 
-    execute_command_args("./add_initially_on_time", [('%s' % PFILE), ('%s' % AT), ('%s' % OBJECT)])
+        # Output CSV filenames
+        csv_filename = "times.csv"
+        csv_filename_better = "timesDispBetter.csv"
 
-    execute_command_args("./run-planner-to-get-initial-plan", [('%s' % DOMAIN), f"ontime-pfile{PFILE_N}"])
+        # Base filename pattern
+        base_filename = f"{PFILE_N}-"
 
-    execute_command_args('./gen', [('%s' % PFILE_N), ('%s' % DOMAIN), ('%s' % AT), ('%s' % OBJECT)])
+        execute_command_args("./add_initially_on_time", [('%s' % PFILE), ('%s' % AT), ('%s' % OBJECT)])
 
-    if FOREST_DEADLINES_ENABLED:
-        replace_deadlines(PFILE_N)
-        print(f"Deadlines replaced successfully with random forest model prediction deadlines..")
+        execute_command_args("./run-planner-to-get-initial-plan", [('%s' % DOMAIN), f"ontime-pfile{PFILE_N}"])
 
-    print(f"[{datetime.now()}] Finished running the generation script.")
+        execute_command_args('./gen', [('%s' % PFILE_N), ('%s' % DOMAIN), ('%s' % AT), ('%s' % OBJECT)])
 
-    base_command_common = ("./rewrite-no-lp --time-based-on-expansions-per-second 500 "
-                           "--include-metareasoning-time --multiply-TILs-by 1 "
-                           "--real-to-plan-time-multiplier 1 --calculate-Q-interval 100 "
-                           "--add-weighted-f-value-to-Q -0.000001 --min-probability-failure 0.001 "
-                           "--slack-from-heuristic --forbid-self-overlapping-actions "
-                           "--deadline-aware-open-list IJCAI --ijcai-gamma 1 --ijcai-t_u 100 "
-                           "--icaps-for-n-expansions 100 --time-aware-heuristic 1 "
-                           "--dispatch-frontier-size 10 --subtree-focus-threshold 0.025 "
-                           "--dispatch-threshold 0.025 --optimistic-lst-for-dispatch-reasoning ")
+        if FOREST_DEADLINES_ENABLED:
+            replace_deadlines(PFILE_N)
+            print(f"Deadlines replaced successfully with random forest model prediction deadlines..")
 
-    base_command_end = (f" %s withdeadlines-ontime-pfile{PFILE_N}-" % DOMAIN)
+        print(f"[{datetime.now()}] Finished running the generation script.")
 
+        base_command_common = ("./rewrite-no-lp --time-based-on-expansions-per-second 500 "
+                               "--include-metareasoning-time --multiply-TILs-by 1 "
+                               "--real-to-plan-time-multiplier 1 --calculate-Q-interval 100 "
+                               "--add-weighted-f-value-to-Q -0.000001 --min-probability-failure 0.001 "
+                               "--slack-from-heuristic --forbid-self-overlapping-actions "
+                               "--deadline-aware-open-list IJCAI --ijcai-gamma 1 --ijcai-t_u 100 "
+                               "--icaps-for-n-expansions 100 --time-aware-heuristic 1 "
+                               "--dispatch-frontier-size 10 --subtree-focus-threshold 0.025 "
+                               "--dispatch-threshold 0.025 --optimistic-lst-for-dispatch-reasoning ")
 
-    # Function to run the dispscript commands
-    def run_dispscript(i):
-        command = base_command_common + "--use-dispatcher LPFThreshold " + base_command_end + str(
-            i) + f" > disp/{PFILE_N}-" + str(i)
-        print(f"[{datetime.now()}] Running disp command for file {PFILE_N}-{i}...")
-        run_subprocess(command, i)
-        print(f"[{datetime.now()}] Finished disp command for file {PFILE_N}-{i}.")
+        base_command_end = (f" %s withdeadlines-ontime-pfile{PFILE_N}-" % DOMAIN)
 
 
-    def run_nodispscript(i):
-        command = base_command_common + base_command_end + str(i) + f" > nodisp/{PFILE_N}-" + str(i)
-        print(f"[{datetime.now()}] Running nodisp command for file {PFILE_N}-{i}...")
-        run_subprocess(command, i)
-        print(f"[{datetime.now()}] Finished nodisp command for file {PFILE_N}-{i}.")
+        # Function to run the dispscript commands
+        def run_dispscript(i):
+            command = base_command_common + "--use-dispatcher LPFThreshold " + base_command_end + str(
+                i) + f" > disp/{PFILE_N}-" + str(i)
+            print(f"[{datetime.now()}] Running disp command for file {PFILE_N}-{i}...")
+            run_subprocess(command, i)
+            print(f"[{datetime.now()}] Finished disp command for file {PFILE_N}-{i}.")
 
 
-    def run_subprocess(command, i):
-        stdout, stderr = None, None  # Initialize these to avoid UnboundLocalError
+        def run_nodispscript(i):
+            command = base_command_common + base_command_end + str(i) + f" > nodisp/{PFILE_N}-" + str(i)
+            print(f"[{datetime.now()}] Running nodisp command for file {PFILE_N}-{i}...")
+            run_subprocess(command, i)
+            print(f"[{datetime.now()}] Finished nodisp command for file {PFILE_N}-{i}.")
 
-        # Start the process in a new session
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                   preexec_fn=os.setsid)
-        try:
-            # communicate() waits for the process to complete or for the timeout to expire
-            stdout, stderr = process.communicate(timeout=PLAN_SEARCH_TIMEOUT_SECONDS)
-        except subprocess.TimeoutExpired:
-            # If the timeout expires, kill the entire process group
-            os.killpg(os.getpgid(process.pid), signal.SIGTERM)  # try to terminate the process group gracefully
-            process.wait(timeout=10)  # give it 10 seconds to terminate gracefully
-            if process.poll() is None:  # if the process is still running after 10 seconds
-                os.killpg(os.getpgid(process.pid), signal.SIGKILL)  # forcibly kill the process group
-            print(f"Command #{i} took longer than timeout seconds and was killed!")
-        else:
-            if process.returncode != 0:
-                print(f"Command #{i} failed!")
+
+        def run_subprocess(command, i):
+            stdout, stderr = None, None  # Initialize these to avoid UnboundLocalError
+
+            # Start the process in a new session
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                       preexec_fn=os.setsid)
+            try:
+                # communicate() waits for the process to complete or for the timeout to expire
+                stdout, stderr = process.communicate(timeout=PLAN_SEARCH_TIMEOUT_SECONDS)
+            except subprocess.TimeoutExpired:
+                # If the timeout expires, kill the entire process group
+                os.killpg(os.getpgid(process.pid), signal.SIGTERM)  # try to terminate the process group gracefully
+                process.wait(timeout=10)  # give it 10 seconds to terminate gracefully
+                if process.poll() is None:  # if the process is still running after 10 seconds
+                    os.killpg(os.getpgid(process.pid), signal.SIGKILL)  # forcibly kill the process group
+                print(f"Command #{i} took longer than timeout seconds and was killed!")
             else:
-                print(f"Command #{i} completed successfully!")
+                if process.returncode != 0:
+                    print(f"Command #{i} failed!")
+                else:
+                    print(f"Command #{i} completed successfully!")
 
-        # Return stdout, stderr, and returncode
-        return stdout, stderr, process.returncode
+            # Return stdout, stderr, and returncode
+            return stdout, stderr, process.returncode
 
-    # Define the number of processes to spawn. Ideally, this is the number of cores available.
-    num_processes = multiprocessing.cpu_count() if ENABLE_PARALLEL else 1
-    # Log the number of processes
-    print(f"[{datetime.now()}] Running with {num_processes} parallel processes.")
+        # Define the number of processes to spawn. Ideally, this is the number of cores available.
+        num_processes = multiprocessing.cpu_count() if ENABLE_PARALLEL else 1
+        # Log the number of processes
+        print(f"[{datetime.now()}] Running with {num_processes} parallel processes.")
 
-    # Execute the scripts
-    if ENABLE_PARALLEL:
-        with multiprocessing.Pool(processes=num_processes) as pool:
-            pool.map(run_dispscript, range(1, EXPERIMENTS + 1))  # This will distribute the list of indexes to the available processes
-            pool.map(run_nodispscript, range(1, EXPERIMENTS + 1))
-    else:
-        # Non-parallel execution as fallback
-        for i in range(1, EXPERIMENTS + 1):
-            run_dispscript(i)
-            run_nodispscript(i)
+        # Execute the scripts
+        if ENABLE_PARALLEL:
+            with multiprocessing.Pool(processes=num_processes) as pool:
+                pool.map(run_dispscript, range(1, EXPERIMENTS + 1))  # This will distribute the list of indexes to the available processes
+                pool.map(run_nodispscript, range(1, EXPERIMENTS + 1))
+        else:
+            # Non-parallel execution as fallback
+            for i in range(1, EXPERIMENTS + 1):
+                run_dispscript(i)
+                run_nodispscript(i)
 
-    # Extract time from the line
-    def extract_time(line):
-        try:
-            return float(line.split()[2])
-        except:
-            # parsing code error
-            return "9999999"
+        # Extract time from the line
+        def extract_time(line):
+            try:
+                return float(line.split()[2])
+            except:
+                # parsing code error
+                return "9999999"
 
 
-    # Extract execution time data and write to CSV
-    def extract_time_and_write_csv():
-        with open(csv_filename, 'w', newline='') as csvfile, open(csv_filename_better, 'w',
-                                                                  newline='') as csvbetterfile:
-            csvwriter = csv.writer(csvfile)
-            csvbetterwriter = csv.writer(csvbetterfile)
-            csvwriter.writerow(['File', 'disp', 'nodisp'])
-            csvbetterwriter.writerow(['File', 'disp', 'nodisp'])
+        # Extract execution time data and write to CSV
+        def extract_time_and_write_csv():
+            with open(csv_filename, 'w', newline='') as csvfile, open(csv_filename_better, 'w',
+                                                                      newline='') as csvbetterfile:
+                csvwriter = csv.writer(csvfile)
+                csvbetterwriter = csv.writer(csvbetterfile)
+                csvwriter.writerow(['File', 'disp', 'nodisp'])
+                csvbetterwriter.writerow(['File', 'disp', 'nodisp'])
 
-            for i in range(1, EXPERIMENTS + 1):  # 1 to 100 inclusive
-                filename = base_filename + str(i)
-                times = [filename]  # Start with filename as first column
+                for i in range(1, EXPERIMENTS + 1):  # 1 to 100 inclusive
+                    filename = base_filename + str(i)
+                    times = [filename]  # Start with filename as first column
 
-                disp_time = 0
-                nodisp_time = 0
+                    disp_time = 0
+                    nodisp_time = 0
 
-                for directory in directories:
-                    filepath = os.path.join(directory, filename)
+                    for directory in directories:
+                        filepath = os.path.join(directory, filename)
 
-                    if os.path.exists(filepath):
-                        with open(filepath, 'r') as f:
+                        if os.path.exists(filepath):
+                            with open(filepath, 'r') as f:
 
-                            lines = f.readlines()
+                                lines = f.readlines()
 
-                            # ;;;; Problem Unsolvable
-                            line_unsolvable = next((l for l in lines if l.startswith(";;;; Problem Unsolvable")), None)
-                            if not line_unsolvable:
-                                # Search in the stored lines for the line with '; Time'
-                                line = next((l for l in lines if l.startswith("; Time")), None)
+                                # ;;;; Problem Unsolvable
+                                line_unsolvable = next((l for l in lines if l.startswith(";;;; Problem Unsolvable")), None)
+                                if not line_unsolvable:
+                                    # Search in the stored lines for the line with '; Time'
+                                    line = next((l for l in lines if l.startswith("; Time")), None)
 
-                                if line:
-                                    time_val = extract_time(line)
-                                    if directory == 'disp':
-                                        disp_time = time_val
+                                    if line:
+                                        time_val = extract_time(line)
+                                        if directory == 'disp':
+                                            disp_time = time_val
+                                        else:
+                                            nodisp_time = time_val
+                                        times.append(time_val)
                                     else:
-                                        nodisp_time = time_val
-                                    times.append(time_val)
+                                        # no line for time code
+                                        times.append(9999)
                                 else:
-                                    # no line for time code
-                                    times.append(9999)
-                            else:
-                                # unsolvable code
-                                times.append(99999)
-                    else:
-                        # filepath doesn't exist code
-                        times.append(999999)
+                                    # unsolvable code
+                                    times.append(99999)
+                        else:
+                            # filepath doesn't exist code
+                            times.append(999999)
 
-                # Write extracted times to CSV
-                csvwriter.writerow(times)
+                    # Write extracted times to CSV
+                    csvwriter.writerow(times)
 
-                # If 'disp' is faster than 'nodisp', write to the better times file
-                if isinstance(disp_time, float) and isinstance(nodisp_time, float) and disp_time < nodisp_time:
-                    csvbetterwriter.writerow(times)
+                    # If 'disp' is faster than 'nodisp', write to the better times file
+                    if isinstance(disp_time, float) and isinstance(nodisp_time, float) and disp_time < nodisp_time:
+                        csvbetterwriter.writerow(times)
 
 
-    extract_time_and_write_csv()
+        extract_time_and_write_csv()
 
-    # Read the times.csv file
-    with open('times.csv', 'r') as file:
-        reader = csv.reader(file)
+        # Read the times.csv file
+        with open('times.csv', 'r') as file:
+            reader = csv.reader(file)
 
-        # Extract headers and rows
-        headers = next(reader)
-        rows = [row for row in reader if float(row[1]) < float(row[2])]
+            # Extract headers and rows
+            headers = next(reader)
+            rows = [row for row in reader if float(row[1]) < float(row[2])]
 
-    # Write the filtered rows to timesDispBetter.csv
-    with open('timesDispBetter.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(headers)
-        writer.writerows(rows)
+        # Write the filtered rows to timesDispBetter.csv
+        with open('timesDispBetter.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(headers)
+            writer.writerows(rows)
 
-    print(f"[{datetime.now()}] Finished current set of experiments. Starting archiving...")
+        print(f"[{datetime.now()}] Finished current set of experiments. Starting archiving...")
 
-    # Once the script finishes its run
-    create_archive()
-    print(f"[{datetime.now()}] Finished archiving.")
+        # Once the script finishes its run
+        create_archive()
+        print(f"[{datetime.now()}] Finished archiving.")
 
-    print(f"[{datetime.now()}] Starting removing cache folder and files.")
-    remove_folders_and_files()
-    print(f"[{datetime.now()}] Finished removing cache folder and files.")
+        print(f"[{datetime.now()}] Starting removing cache folder and files.")
+        remove_folders_and_files()
+        print(f"[{datetime.now()}] Finished removing cache folder and files.")
+
+        print(f"[{datetime.now()}] All PFILE_N experiments completed for this cycle. Restarting...")
