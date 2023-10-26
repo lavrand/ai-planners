7d0897672cb21e6e4c49041417cfdd24b9a76621
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2023-10-26 16:47:09 +0300

    analyze general report improved, generic plots added

--- _analize_general.py
+++ _analize_general.py
@@ -4,10 +4,10 @@ import subprocess
 import pandas as pd
 from docx import Document
 from docx.shared import Inches
-import subprocess
+import matplotlib.pyplot as plt
 
 # Get the total number of archive folders (assuming they are sequentially named and start from 1)
-N = 22  # Set this to the actual number of archive folders you have
+N = 20  # Set this to the actual number of archive folders you have
 
 # List of scripts to copy
 scripts = [
@@ -53,9 +53,57 @@ for i in range(1, N + 1):
     for file_name in ['disp_vs_nodisp_plot.png', 'summary.csv']:
         shutil.copy(os.path.join(folder_name, file_name), os.path.join(new_folder_path, file_name))
 
+def generate_plots(summary_csv_path):
+    # Load the data
+    data = pd.read_csv(summary_csv_path)
+
+    # Data preparation
+    labels = data['Unnamed: 0']
+    avg_time_disp = data['Average solution time for disp on permutations solved by both disp and nodisp']
+    avg_time_nodisp = data['Average solution time for nodisp on permutations solved by both disp and nodisp']
+    both_solved = data['Number of permutations solved by both disp and nodisp']
+    only_disp_solved = data['Number of permutations solved only by disp']
+    only_nodisp_solved = data['Number of permutations solved only by nodisp']
+
+    # --- Visualization 1: Average Solution Time Comparison ---
+    plt.figure(figsize=(14, 7))
+    width = 0.35
+    x = range(len(labels))
+    x_disp = [i - width/2 for i in x]
+    x_nodisp = [i + width/2 for i in x]
+    plt.bar(x_disp, avg_time_disp, width, label='Disp', color='green')
+    plt.bar(x_nodisp, avg_time_nodisp, width, label='NoDisp', color='orange')
+    plt.xlabel('Problem Files')
+    plt.ylabel('Average Solution Time (in units)')
+    plt.title('Average Solution Time Comparison Between Disp and NoDisp')
+    plt.xticks(ticks=x, labels=labels, rotation=45)
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('plot1.png')  # Save the plot to a file
+
+    # --- Visualization 2: Comparison of Permutations Solved by Method ---
+    plt.figure(figsize=(12, 6))
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x, both_solved, width, label='Both', color='#1f77b4')
+    rects2 = ax.bar(x, only_disp_solved, width, label='Only Disp', bottom=both_solved, color='#2ca02c')
+    rects3 = ax.bar(x, only_nodisp_solved, width, label='Only NoDisp', bottom=both_solved+only_disp_solved, color='#ff7f0e')
+    ax.set_xlabel('Problem Files')
+    ax.set_ylabel('Number of Permutations Solved')
+    ax.set_title('Comparison of Permutations Solved by Method')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels, rotation=45)
+    ax.legend()
+    plt.tight_layout()
+    plt.savefig('plot2.png')  # Save the plot to a file
 
 def generate_doc(summary_dir):
     doc = Document()
+
+    doc.add_heading('Analysis Report', 0)
+
+    # Add plots to the document
+    doc.add_picture('plot1.png', width=Inches(6))
+    doc.add_picture('plot2.png', width=Inches(6))
 
     for i in range(1, N + 1):
         new_folder_name = f'pfile_{i}'
@@ -79,12 +127,6 @@ def generate_doc(summary_dir):
     doc.save(doc_path)
 
     return doc_path
-
-# After copying files to the new folders in the summary directory
-doc_path = generate_doc(summary_dir)
-
-# Open the document
-os.system(f'xdg-open {doc_path}')
 
 def generate_summary_csv(summary_dir):
     all_data = []  # List to hold data from all CSV files
@@ -111,6 +153,15 @@ def generate_summary_csv(summary_dir):
 
 # After processing all folders or at the end of your script
 summary_csv_path = generate_summary_csv(summary_dir)
+
+# Generate plots
+generate_plots(summary_csv_path)
+
+# After copying files to the new folders in the summary directory
+doc_path = generate_doc(summary_dir)
+
+# Open the document
+os.system(f'xdg-open {doc_path}')
 
 # Optionally, open the summary CSV file
 subprocess.run(['xdg-open', summary_csv_path])
