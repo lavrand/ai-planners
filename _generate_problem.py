22871e7c0ddb2a0763b3e867a40f266e374e60b8
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2023-11-13 21:29:07 +0200

    pddl wip

--- _generate_problem.py
+++ _generate_problem.py
@@ -1,33 +1,61 @@
 import random
 
-def generate_complex_pddl_problem(num_locations, output_file):
-    locations = [f"loc{i}" for i in range(1, num_locations + 1)]
+def generate_domain_pddl(domain_file):
+    with open(domain_file, "w") as file:
+        file.write("""(define (domain complex_navigation)
+    (:requirements :strips :typing)
+    (:types 
+        location
+        dead_end
+    )
+    (:predicates
+        (path ?from - location ?to - location)
+        (dead_end_path ?from - location ?to - dead_end)
+        (at ?loc - location)
+    )
+    (:action move
+        :parameters (?from ?to - location)
+        :precondition (and (at ?from) (path ?from ?to))
+        :effect (and (not (at ?from)) (at ?to))
+    )
+    (:action move_to_dead_end
+        :parameters (?from - location ?to - dead_end)
+        :precondition (and (at ?from) (dead_end_path ?from ?to))
+        :effect (and (not (at ?from)))
+    )
+)""")
+    print(f"Domain file generated: {domain_file}")
 
-    with open(output_file, "w") as file:
+def generate_problem_pddl(num_locations, num_dead_ends, problem_file):
+    locations = [f"loc{i}" for i in range(1, num_locations + 1)]
+    dead_ends = [f"dead{i}" for i in range(1, num_dead_ends + 1)]
+
+    with open(problem_file, "w") as file:
         file.write("(define (problem extended_navigation_problem)\n")
         file.write("  (:domain complex_navigation)\n")
         file.write("  (:objects\n    ")
-        file.write(" ".join(locations))
+        file.write(" ".join(locations + dead_ends))
         file.write(" - location\n  )\n")
         file.write("  (:init\n    (at loc1)\n")
 
-        # Creating a dense and complex network of paths
+        # Paths between locations
         for loc in locations:
-            connections = random.sample(locations, random.randint(5, 10))  # Increase connections per location
+            connections = random.sample(locations, random.randint(2, 4))
             for conn in connections:
                 if conn != loc:
                     file.write(f"    (path {loc} {conn})\n")
 
-        # Introducing dead ends
-        for _ in range(num_locations // 2):
-            dead_end_from = random.choice(locations)
-            dead_end_to = f"dead{random.randint(1, num_locations)}"
-            file.write(f"    (path {dead_end_from} {dead_end_to})\n")
+        # Paths to dead ends
+        for loc in locations:
+            dead_end_conn = random.choice(dead_ends)
+            file.write(f"    (dead_end_path {loc} {dead_end_conn})\n")
 
         file.write("  )\n")
         file.write(f"  (:goal\n    (at loc{num_locations})\n  )\n")
         file.write(")\n")
 
-    print(f"Problem file generated: {output_file}")
+    print(f"Problem file generated: {problem_file}")
 
-generate_complex_pddl_problem(150, "pfile1")  # Example with 150 locations
+# Generate domain and problem files
+generate_domain_pddl("domain.pddl")
+generate_problem_pddl(150, 150, "pfile1")  # Adjust numbers as needed
