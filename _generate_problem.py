2f462559c25e2bd12136528626dc0d0d6725edc6
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2023-11-13 21:12:01 +0200

    generate problem improved

--- _generate_problem.py
+++ _generate_problem.py
@@ -11,19 +11,12 @@ def generate_pddl_problem(num_locations, output_file):
         file.write(" - location\n  )\n")
         file.write("  (:init\n    (at loc1)\n")
 
-        # Creating a guaranteed solvable main path
-        for i in range(1, num_locations):
-            file.write(f"    (path loc{i} loc{i+1})\n")
-
-        # Adding extra paths for complexity
+        # Creating paths
         all_possible_paths = set((f"loc{i}", f"loc{j}") for i in range(1, num_locations) for j in range(1, num_locations) if i != j)
-        main_path = set((f"loc{i}", f"loc{i+1}") for i in range(1, num_locations))
-        extra_paths = all_possible_paths - main_path
+        chosen_paths = set(random.sample(all_possible_paths, num_locations * 3))  # Increase the number of paths
 
-        for _ in range(num_locations * 2):  # Limit the number of extra paths
-            from_loc, to_loc = random.sample(extra_paths, 1)[0]
+        for from_loc, to_loc in chosen_paths:
             file.write(f"    (path {from_loc} {to_loc})\n")
-            extra_paths.remove((from_loc, to_loc))
 
         file.write("  )\n")
         file.write(f"  (:goal\n    (at loc{num_locations})\n  )\n")
@@ -31,4 +24,5 @@ def generate_pddl_problem(num_locations, output_file):
 
     print(f"Problem file generated: {output_file}")
 
-generate_pddl_problem(50, "pfile1")
+# Increase the number of locations to increase complexity
+generate_pddl_problem(100, "pfile")  # Example with 100 locations
