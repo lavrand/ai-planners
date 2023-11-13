911cb55f4cfb9fd59615886fa83dd63063cc1ed8
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2023-11-13 21:07:41 +0200

    generate problem improved

--- _generate_problem.py
+++ _generate_problem.py
@@ -11,16 +11,19 @@ def generate_pddl_problem(num_locations, output_file):
         file.write(" - location\n  )\n")
         file.write("  (:init\n    (at loc1)\n")
 
-        # Creating a guaranteed solvable path
+        # Creating a guaranteed solvable main path
         for i in range(1, num_locations):
             file.write(f"    (path loc{i} loc{i+1})\n")
 
-        # Adding additional random paths
-        for loc in locations[:-1]:  # Exclude the last location
-            connections = random.sample(locations, random.randint(2, 4))
-            for conn in connections:
-                if conn != loc:
-                    file.write(f"    (path {loc} {conn})\n")
+        # Adding extra paths for complexity
+        all_possible_paths = set((f"loc{i}", f"loc{j}") for i in range(1, num_locations) for j in range(1, num_locations) if i != j)
+        main_path = set((f"loc{i}", f"loc{i+1}") for i in range(1, num_locations))
+        extra_paths = all_possible_paths - main_path
+
+        for _ in range(num_locations * 2):  # Limit the number of extra paths
+            from_loc, to_loc = random.sample(extra_paths, 1)[0]
+            file.write(f"    (path {from_loc} {to_loc})\n")
+            extra_paths.remove((from_loc, to_loc))
 
         file.write("  )\n")
         file.write(f"  (:goal\n    (at loc{num_locations})\n  )\n")
@@ -28,4 +31,4 @@ def generate_pddl_problem(num_locations, output_file):
 
     print(f"Problem file generated: {output_file}")
 
-generate_pddl_problem(50, "pfile.pddl")
+generate_pddl_problem(50, "pfile1")
