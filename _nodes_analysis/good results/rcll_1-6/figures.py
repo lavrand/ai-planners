6d606377e37b6f5e089b6234ff6e91ef7f79779f
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2023-11-23 02:32:51 +0200

    graphs font sized increased

--- _nodes_analysis/good results/rcll_1-6/figures.py
+++ _nodes_analysis/good results/rcll_1-6/figures.py
@@ -12,10 +12,11 @@ nodes_expanded_data.columns = nodes_expanded_data.columns.str.replace('disp', 'D
 # Plotting the time taken by the Dispatcher approach for each problem instance
 plt.figure(figsize=(20, 10))
 plt.bar(time_data['Identifier'], time_data['Dispatcher'], color='blue')
-plt.xlabel('Problem Instance')
-plt.ylabel('Time (Seconds)')
-plt.title('Time Taken by Dispatcher Approach for Each Problem Instance')
-plt.xticks(rotation=90)
+plt.xlabel('Problem Instance', fontsize=14)
+plt.ylabel('Time (Seconds)', fontsize=14)
+plt.title('Time Taken by Dispatcher Approach for Each Problem Instance', fontsize=20)
+plt.xticks(rotation=90, fontsize=16)
+plt.yticks(fontsize=16)
 plt.grid(True)
 plt.tight_layout()
 
@@ -24,13 +25,14 @@ time_taken_dispatcher_path = 'time_taken_disp.png'
 plt.savefig(time_taken_dispatcher_path)
 plt.close()
 
-# Plotting the nodes expanded by the Dispatcher approach for each problem instance
+# For the Nodes Expanded graph
 plt.figure(figsize=(20, 10))
 plt.bar(nodes_expanded_data['Identifier'], nodes_expanded_data['Dispatcher'], color='green')
-plt.xlabel('Problem Instance')
-plt.ylabel('Nodes Expanded')
-plt.title('Nodes Expanded by Dispatcher Approach for Each Problem Instance')
-plt.xticks(rotation=90)
+plt.xlabel('Problem Instance', fontsize=14)
+plt.ylabel('Nodes Expanded', fontsize=14)
+plt.title('Nodes Expanded by Dispatcher Approach for Each Problem Instance', fontsize=20)
+plt.xticks(rotation=90, fontsize=16)
+plt.yticks(fontsize=16)
 plt.grid(True)
 plt.tight_layout()
 
@@ -39,5 +41,4 @@ nodes_expanded_dispatcher_path = 'nodes_expanded_disp.png'
 plt.savefig(nodes_expanded_dispatcher_path)
 plt.close()
 
-# Returning the file paths
 time_taken_dispatcher_path, nodes_expanded_dispatcher_path
