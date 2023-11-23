fffc8e2b84ac8ea6edc48be210b0a9c44207cd9e
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2023-11-23 03:06:28 +0200

    overleaf and figures wip

--- _nodes_analysis/good results/rcll_1-6/figures.py
+++ _nodes_analysis/good results/rcll_1-6/figures.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-TITLE_FONTSIZE = 50
+FIGSIZE_Y = 20
+FIGSIZE_X = 30
 
-FONTSIZE = 40
+# Constants for font sizes
+TITLE_FONTSIZE = 50
+AXIS_FONTSIZE = 40
+TICKS_FONTSIZE = 40
 
 # Load the data from CSV files
 time_data = pd.read_csv('Time.csv')
@@ -13,14 +17,14 @@ nodes_expanded_data = pd.read_csv('Nodes_Expanded.csv')
 time_data.columns = time_data.columns.str.replace('disp', 'Dispatcher')
 nodes_expanded_data.columns = nodes_expanded_data.columns.str.replace('disp', 'Dispatcher')
 
-# Plotting the time taken by the Dispatcher approach for each problem instance
-plt.figure(figsize=(TITLE_FONTSIZE, 10))
+# Plotting the time taken by Dispatcher approach for each problem instance with improved figure size
+plt.figure(figsize=(FIGSIZE_X, FIGSIZE_Y))  # Setting figure size
 plt.bar(time_data['Identifier'], time_data['Dispatcher'], color='blue')
-plt.xlabel('Problem Instance', fontsize=FONTSIZE)
-plt.ylabel('Time (Seconds)', fontsize=FONTSIZE)
+plt.xlabel('Problem Instance', fontsize=AXIS_FONTSIZE)
+plt.ylabel('Time (Seconds)', fontsize=AXIS_FONTSIZE)
 plt.title('Time Taken by Dispatcher Approach for Each Problem Instance', fontsize=TITLE_FONTSIZE)
-plt.xticks(rotation=90, fontsize=FONTSIZE)
-plt.yticks(fontsize=FONTSIZE)
+plt.xticks(rotation=90, fontsize=TICKS_FONTSIZE)
+plt.yticks(fontsize=TICKS_FONTSIZE)
 plt.grid(True)
 plt.tight_layout()
 
@@ -29,14 +33,14 @@ time_taken_dispatcher_path = 'time_taken_disp.png'
 plt.savefig(time_taken_dispatcher_path)
 plt.close()
 
-# For the Nodes Expanded graph
-plt.figure(figsize=(TITLE_FONTSIZE, 10))
+# Plotting the nodes expanded by Dispatcher approach for each problem instance with improved figure size
+plt.figure(figsize=(FIGSIZE_X, FIGSIZE_Y))  # Setting figure size
 plt.bar(nodes_expanded_data['Identifier'], nodes_expanded_data['Dispatcher'], color='green')
-plt.xlabel('Problem Instance', fontsize=FONTSIZE)
-plt.ylabel('Nodes Expanded', fontsize=FONTSIZE)
+plt.xlabel('Problem Instance', fontsize=AXIS_FONTSIZE)
+plt.ylabel('Nodes Expanded', fontsize=AXIS_FONTSIZE)
 plt.title('Nodes Expanded by Dispatcher Approach for Each Problem Instance', fontsize=TITLE_FONTSIZE)
-plt.xticks(rotation=90, fontsize=FONTSIZE)
-plt.yticks(fontsize=FONTSIZE)
+plt.xticks(rotation=90, fontsize=TICKS_FONTSIZE)
+plt.yticks(fontsize=TICKS_FONTSIZE)
 plt.grid(True)
 plt.tight_layout()
 
