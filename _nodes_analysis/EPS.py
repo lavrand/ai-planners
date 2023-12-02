836e3ffe545741ebc8996ce8dfa6ef3ca5eefb9b
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2023-12-03 00:49:13 +0200

    EPS wip

--- _nodes_analysis/EPS.py
+++ _nodes_analysis/EPS.py
@@ -1,19 +1,25 @@
 import pandas as pd
 import numpy as np
 
-def calculate_log_etps_updated(nodes_expanded_file, time_file):
+
+def calculate_log_etps_updated(nodes_expanded_file, time_file, solution_file):
     # Read the CSV files
     nodes_expanded_df = pd.read_csv(nodes_expanded_file)
     time_df = pd.read_csv(time_file)
+    solution_df = pd.read_csv(solution_file)
+
+    # Filter out identifiers without solutions
+    solution_df = solution_df[(solution_df['disp'] == 'Solution Found') | (solution_df['nodisp'] == 'Solution Found')]
 
     # Merging the dataframes on 'Identifier'
     merged_df = pd.merge(nodes_expanded_df, time_df, on='Identifier', suffixes=('_nodes', '_time'))
+    merged_df = pd.merge(merged_df, solution_df, on='Identifier')
 
     # Calculate Expanded Time Per Second (ETPS) for 'disp' and 'nodisp', while handling NaNs or 0 values
     merged_df['ETPS_disp'] = merged_df.apply(lambda row: row['disp_nodes'] / row['disp_time']
-                                             if row['disp_time'] > 0 else np.nan, axis=1)
+    if row['disp_time'] > 0 else np.nan, axis=1)
     merged_df['ETPS_nodisp'] = merged_df.apply(lambda row: row['nodisp_nodes'] / row['nodisp_time']
-                                               if row['nodisp_time'] > 0 else np.nan, axis=1)
+    if row['nodisp_time'] > 0 else np.nan, axis=1)
 
     # Concatenating the ETPS values from both 'disp' and 'nodisp', ignoring NaNs
     all_etps_values = pd.concat([merged_df['ETPS_disp'], merged_df['ETPS_nodisp']]).dropna()
@@ -26,8 +32,10 @@ def calculate_log_etps_updated(nodes_expanded_file, time_file):
 
     return log_overall_average_etps
 
+
 # Example usage
 nodes_expanded_file = 'Nodes_Expanded.csv'  # Replace with your file path
 time_file = 'Time.csv'  # Replace with your file path
-log_etps = calculate_log_etps_updated(nodes_expanded_file, time_file)
-print(f"Overall Average EPS: {log_etps}")
+solution_file = 'Solution.csv'  # Replace with your file path
+log_etps = calculate_log_etps_updated(nodes_expanded_file, time_file, solution_file)
+print(f"Log of Overall Average ETPS: {log_etps}")
