af45a252335497881e785b807c575f846a86f8e1
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2023-12-04 00:03:18 +0200

    _run_configs.sh supports last line without enter

--- _run_configs.sh
+++ _run_configs.sh
@@ -7,8 +7,7 @@ CONFIGS_FILE="_configs/configs.ini"
 echo "Starting script at $(date)" > $LOGFILE
 
 # Read each line in configs.ini and use it as a config file name
-while IFS= read -r config
-do
+while IFS= read -r config || [[ -n "$config" ]]; do
    echo "Starting main.py with $config at $(date)" >> $LOGFILE
    python main.py _configs/$config &
    echo "Started main.py with $config at $(date)" >> $LOGFILE
