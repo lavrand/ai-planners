a8ca90bf6e4271661b66777b3de7109c03cb6197
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2023-12-05 00:34:00 +0200

    rcll old wip

--- _configs/_update_configs.py
+++ _configs/_update_configs.py
@@ -5,7 +5,7 @@ TOTAL_INPUT = 300  # Total input value
 N = 12              # Number of configuration files
 
 # New constants to replace
-TIME_BASED_ON_EXPANSIONS_PER_SECOND = 500
+TIME_BASED_ON_EXPANSIONS_PER_SECOND = 1000
 PLAN_SEARCH_TIMEOUT_SECONDS = 300
 DOMAIN = "rcll_domain_production_durations_time_windows.pddl"
 SUBTREE_FOCUS_THRESHOLD = 1
